8de913d9a8643a7d65a14b6c782af2c70d4fe621
반현랑 <hyunlang@banhyeonlang-ui-MacBookAir.local>
2020-07-15 14:19:54 +0900

    로그인 데코레이터 추가

--- user/decorator.py
+++ user/decorator.py
@@ -1,23 +1,24 @@
 import jwt
 import json
-from django.http import jsonresponse
-from user.models import user
+from django.http import JsonResponse
+from user.models import User
 from wenb.settings import SECRET_KEY, ALGORITHM
 
 def login_check(func):
     def wrapper(self, request, *args, **kwargs):
-        token = request.headers.get('authorization', none)
-        if token is not none:
+        token = request.headers.get('authorization', None)
+        if token is not None:
             try:
-                decoded_token = jwt.decode(token, secret, algorithms = algorithm)
+                decoded_token = jwt.decode(token, SECRET_KEY, algorithms = ALGORITHM)
                 user_id       = decoded_token['user_id']
-                if user.objects.get(id=user_id):
-                    user         = user.objects.get(id=user_id)
+                print(user_id)
+                if User.objects.get(id=user_id):
+                    user         = User.objects.get(id=user_id)
                     request.user = user
                     return func(self, request, *args, **kwargs)
-            except jwt.decodeerror:
-                return jsonresponse({'message': 'invalid_token'}, status = 400)
-            except user.doesnotexist:
-                return jsonresponse({'message': 'invalid_user'}, status = 400)
-        return jsonresponse({'message': 'need_login'}, status = 401)
+            except jwt.DecodeError:
+                return JsonResponse({'message': 'invalid_token'}, status = 400)
+            except User.DoesNotExist:
+                return JsonResponse({'message': 'invalid_user'}, status = 400)
+        return JsonResponse({'message': 'need_login'}, status = 401)
     return wrapper
